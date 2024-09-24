58786ccdcf60a49833b18ba4035b6302f3564163
Profesor-2400 <mavc_2004@hotmail.com>
2024-09-23 22:10:29 -0500

    Segundo Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Hi. Get started by editing <code>src/app/page.tsx</code>.
+            Hi, Welcome To SerFlix. Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
