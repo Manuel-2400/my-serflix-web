8674bb3c534a0f90684cb27e453e3be0967e9546
“Daniel.escobar” <sistemasmi24@gmail.com>
2024-09-24 13:42:20 -0500

    mandar commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,20 @@ export default function Home() {
   // const IMG_PATCH = 'https://image.tmdb.org/t/p/original'
   // const URL_IMG = 'https://image.tmdb.org/t/p/original'
 
+  //Test commit daniel
   fetch('https://api.themoviedb.org/3/movie/11?api_key=49c9cb85300478a6d4052f8f18f2045f')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // Convierte la respuesta a JSON
-  })
-  .then(data => {
-    console.log(data, 'Mira Aqui'); // Trabaja con los datos recibidos de la API
-  })
-  .catch(error => {
-    console.error('Hubo un problema con la solicitud Fetch:', error);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Convierte la respuesta a JSON
+    })
+    .then(data => {
+      console.log(data, 'Mira Aqui'); // Trabaja con los datos recibidos de la API
+    })
+    .catch(error => {
+      console.error('Hubo un problema con la solicitud Fetch:', error);
+    });
 
 
   return (
