a9c3c191a21152b9e117423dd53dbf03932d0d2e
“Daniel.escobar” <sistemasmi24@gmail.com>
2024-11-08 12:28:10 -0500

    feat: implementacion de estilos

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import './Header.css'
 
 export const Header = () => {
   return (
-    <nav>
-      <div className="container">
-        <a href="/" className="logo">SerFlix</a>
+    <header className="header">
+      <a href="/" className="logo">SerFlix</a>
+      <nav className="nav">
         <ul className="nav-links">
-          <li><Link href="#now-playing">Cartelera</Link></li>
-          <li><Link href="#genres">Generos</Link></li>
-          <li><Link href="#popular">Populares</Link></li>
-          <li><Link href="#rated">Mejores Calificados</Link></li>
-          <li><Link href="#soon">Proximamente</Link></li>
-          <li><Link href="/iniciar-sesion">Iniciar sesion</Link></li>
+          <li>
+            <Link href="#now-playing" className="nav-link">Cartelera</Link>
+          </li>
+          <li>
+            <Link href="#genres" className="nav-link">Generos</Link>
+          </li>
+          <li>
+            <Link href="#popular" className="nav-link">Populares</Link>
+          </li>
+          <li>
+            <Link href="#rated" className="nav-link">Mejores Calificados</Link>
+          </li>
+          <li>
+            <Link href="#soon" className="nav-link">Proximamente</Link>
+          </li>
         </ul>
-      </div>
-    </nav>
+        <Link href="/iniciar-sesion" className="nav-link">Iniciar sesion</Link>
+      </nav>
+    </header>
   );
 };
