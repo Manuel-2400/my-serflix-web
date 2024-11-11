2e48a9f8dcfade1f7489f8609b0f980b1879f9f0
Profesor-2400 <mavc_2004@hotmail.com>
2024-11-11 12:00:53 -0500

    Esctrtura Web 2.0 y GitIgnore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,52 @@ export default function Home() {
           Estas logeado
           <Button onClick={login}>Salirte</Button>
 
+          {/* Banner principal */}
+      <section className="banner">
+        <div className="banner-text">
+          <p>12/11/2024</p>
+          <h2>Tu próxima película está a solo una búsqueda de distancia</h2>
+          <input type="text" placeholder="Buscar" />
+        </div>
+        <div className="banner-image">
+          {/* Contenedor para imagen */}
+        </div>
+      </section>
+
+      {/* Cartelera */}
+      <section className="cartelera-section">
+        <h2>Cartelera</h2>
+        <div className="cartelera-items">
+          {/* Aquí agregar cada película */}
+        </div>
+        <button className="more-movies-btn">Más películas</button>
+      </section>
+
+      {/* Contenido Popular */}
+      <section className="popular-content-section">
+        <h2>Contenido Más Popular De La Semana</h2>
+        <p>Obtén acceso exclusivo a lanzamientos anticipados...</p>
+        <button>Ir a más populares</button>
+      </section>
+
+      {/* Mejores Calificados */}
+      <section className="top-rated-section">
+        <h2>Mejores Calificados</h2>
+        <div className="top-rated-items">
+          {/* Aquí agregar cada contenido */}
+        </div>
+        <button className="more-movies-btn">Más películas</button>
+      </section>
+
+      {/* Próximamente */}
+      <section className="coming-soon-section">
+        <h2>Próximamente</h2>
+        <div className="coming-soon-items">
+          {/* Aquí agregar cada película próxima */}
+        </div>
+        <button className="more-movies-btn">Más películas</button>
+      </section>
+
         </section>
       }
     </Main>
--- src/app/popular/page.tsx
+++ src/app/popular/page.tsx
@@ -3,7 +3,22 @@ import { Main } from "@/layout/Main/main"
 const Popular = () =>{
     return(
         <Main>
-            Popular
+        <section className="top-rated-list">
+        <h2>Mejores Calificados</h2>
+        <div className="top-rated">
+          {/* Aquí agregar tarjetas de películas mejor calificadas */}
+          <div className="top-rated-card">
+            <img src="" alt="Poster" />
+            <div className="top-rated-info">
+              <p>Fecha</p>
+              <h3>Título</h3>
+              <p>Descripción...</p>
+              <button>Leer más</button>
+            </div>
+          </div>
+          {/* Repetir div.top-rated-card para cada película */}
+        </div>
+      </section>
         </Main>
     )
 }
