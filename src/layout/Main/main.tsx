06537caf0c68f81deff095b22364514832c93204
Profesor-2400 <mavc_2004@hotmail.com>
2024-11-12 10:55:12 -0500

    Fix:Fondo Color

--- src/layout/Main/main.tsx
+++ src/layout/Main/main.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import { MainProps } from "./types";
 import { Header } from "./subComponents/Header/header";
 import { Footer } from "./subComponents/Footer/footer";
+import './main.css'
 
 export function Main(props: MainProps) {
     const { children } = props;
     return (
-        <div className="main-container">
+        <>
             <Header />
             <main className="main-content">
                 {children}
             </main>
             <Footer />
-        </div>
+        </>
     );
 }
